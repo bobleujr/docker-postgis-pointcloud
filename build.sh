b78716e80ac8e714652788e74cc0e8ce1480b1bf
Paulo Guilherme Tabarro <>
2018-10-17 17:25:15 -0400

    Added pointcoud to docker-postgis

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build -t kartoza/postgis:manual-build .
+docker tag kartoza/postgis:manual-build kartoza/postgis:9.6-2.4
---
+++ setup-pointcloud.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cd /pointcloud
+chmod 700 /etc/postgresql/9.6/main/postgresql.conf
+wget -O pointcloud.tar.gz "https://github.com/pgpointcloud/pointcloud/archive/master.tar.gz" \
+   && apt-get -y install autoconf zlib1g-dev build-essential libpq-dev libxml2-dev postgresql-server-dev-9.6 \
+   && mkdir -p /usr/src/pointcloud \
+   && tar \
+       --extract \
+       --file pointcloud.tar.gz \
+       --directory /usr/src/pointcloud \
+       --strip-components 1 \
+   && rm pointcloud.tar.gz \
+   && cd /usr/src/pointcloud \
+   && ./autogen.sh \
+   && ./configure --with-pgconfig=/usr/bin/pg_config \
+   && make \
+   && make install
+
+ cd /
