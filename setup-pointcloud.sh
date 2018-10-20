7bd5d1b0ce441a6078e8d46aa495ac7f68e36615
Paulo Guilherme Tabarro <>
2018-10-20 14:50:19 -0400

    Added update to fix broken dependencies

--- setup-pointcloud.sh
+++ setup-pointcloud.sh
@@ -3,7 +3,7 @@
 cd /pointcloud
 chmod 700 /etc/postgresql/9.6/main/postgresql.conf
 wget -O pointcloud.tar.gz "https://github.com/pgpointcloud/pointcloud/archive/master.tar.gz" \
-   && apt-get -y install autoconf zlib1g-dev build-essential libpq-dev libxml2-dev postgresql-server-dev-9.6 \
+   && apt-get update && apt-get -y install autoconf zlib1g-dev build-essential libpq-dev libxml2-dev postgresql-server-dev-9.6 \
    && mkdir -p /usr/src/pointcloud \
    && tar \
        --extract \
